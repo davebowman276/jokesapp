77ca75dfbcb85e8a6ca6197aa2e1faba05eacc1c
Davebowman276 <Davebowman276@gmail.com>
2021-01-07 13:54:52 -0600

    created template for chucknorris jokes

---
+++ src/main/java/com/josh/jokesapp/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package com.josh.jokesapp.controllers;
+
+import com.josh.jokesapp.services.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokesService jokesService;
+
+    @Autowired
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokesService.getJoke());
+
+        return "chucknorris";
+
+    }
+}
---
+++ src/main/java/com/josh/jokesapp/services/JokesService.java
@@ -0,0 +1,5 @@
+package com.josh.jokesapp.services;
+
+public interface JokesService {
+    String getJoke();
+}
